b68ac252788ff0445a4bb728d2366e3c5544691d
Alec <rcsmith.alec@gmail.com>
2020-05-20 15:41:52 +0100

    removed onClick functionality breaking PatientsList

--- src/components/PatientsList.js
+++ src/components/PatientsList.js
@@ -17,7 +17,7 @@ class PatientsList extends Component {
 
   handleClick(event) {
     return (
-      <PatientDetails value={event.target.value}/>
+      <PatientDetails/>
     )
   }
 
@@ -26,7 +26,7 @@ class PatientsList extends Component {
       <div>
         <center><h1>Patients List</h1></center>
         {this.state.patients.map((patient) => (
-          <div key={patient._id} className="card" onClick={this.handleClick()}>
+          <div key={patient._id} className="card">
             <div className="card-body">
               <h5 className="card-title">{patient.name}</h5>
               <h6 className="card-subtitle mb-2 text-muted">{patient.email}</h6>
