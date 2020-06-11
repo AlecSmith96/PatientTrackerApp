f73d7e4ef77eb49f79a9615e8cb2988db34fa282
Alec <rcsmith.alec@gmail.com>
2020-06-11 10:51:14 +0100

    Moved Patient details to separate component

--- src/components/PatientDetails.js
+++ src/components/PatientDetails.js
@@ -4,6 +4,7 @@ import './react-confirm-alert.css';
 import { confirmAlert } from 'react-confirm-alert';
 import { Card, Modal, Button } from 'react-bootstrap';
 import AllergiesList from './AllergiesList';
+import PersonalDetails from './PersonalDetails';
 
 class PatientDetails extends Component {
     constructor(props) {
@@ -12,10 +13,18 @@ class PatientDetails extends Component {
             patient: {},
             deletePopupVisible: false,
             showModal: false,
-            allergies: []
+            allergies: [],
+            editDetails: false,
+            email: '',
+            phoneNumber: ''
         }
 
         this.dischargePatient = this.dischargePatient.bind(this);
+        this.handleEmailChange = this.handleEmailChange.bind(this);
+        this.handlePhoneNumberChange = this.handlePhoneNumberChange.bind(this);
+        this.addAllergyField = this.addAllergyField.bind(this);
+        this.addAllergyValue = this.addAllergyValue.bind(this);
+        this.removeAllergyField = this.removeAllergyField.bind(this);
     }
 
     componentDidMount () {
@@ -53,6 +62,27 @@ class PatientDetails extends Component {
         this.render();
     }
 
+    editPersonalDetails = () => {
+        this.setState({ editDetails: true });
+    } 
+
+    sendUpdateRequest(data) {
+
+    }
+
+    saveChanges = () => {
+        console.log(JSON.stringify(this.state.allergies));
+        const data = `{"name": "${this.state.name}", "email": "${this.state.email}", "phoneNumber": "${this.state.phoneNumber}", "dateOfBirth": "${this.state.dateOfBirth}", "allergies": ${JSON.stringify(this.state.allergies)}}`;
+        console.log(data);
+        this.sendUpdateRequest(data);
+        this.props.history.push('/all');
+        this.render();
+    }
+
+    cancelChanges = () => {
+        this.setState({editDetails: false});
+    }
+
     showModal = () => {
         this.setState({ showModal: true });
     };
@@ -60,6 +90,30 @@ class PatientDetails extends Component {
     hideModal = () => {
         this.setState({ showModal: false });
     };
+
+    handleEmailChange(event) {
+        this.setState({
+            email: event.target.value,
+        })
+    }
+    
+    handlePhoneNumberChange(event) {
+        this.setState({phoneNumber: event.target.value})
+    }
+    
+    addAllergyField(allergy) {
+        this.setState({allergies: [...this.state.allergies, '']});
+    }
+    
+    addAllergyValue(event, index) {
+        this.state.allergies[index] = event.target.value;
+        this.setState({allergies: this.state.allergies});
+    }
+    
+    removeAllergyField(index) {
+        this.state.allergies.splice(index, 1);
+        this.setState({allergies: this.state.allergies});
+    }
 
     render() {
         return (
@@ -70,22 +124,28 @@ class PatientDetails extends Component {
                 <h1 className="text-info display-4">{this.state.patient.name}</h1>
                 <Card>
                 <Card.Header className="text-center">
+                    {
+                        this.state.editDetails ?
+                        <div id="cancelButton" className="btn col-lg-1" visible={this.state.editDetails}>
+                            <Button variant="info" onClick={this.cancelChanges} className="form-control">Cancel</Button>
+                        </div>
+                        :
+                        null
+                    }
                     <div id="editDetailsButton" className="btn col-lg-1">
-                        <Button variant="outline-success" onClick={this.showModal} className="form-control">Edit Details</Button>
+                        <Button variant="outline-success" onClick={!this.state.editDetails ? this.editPersonalDetails : this.saveChanges} className="form-control">{!this.state.editDetails ? 'Edit Details' : 'Save Changes'}</Button>
                     </div>
                     <div id="AddMedicationsButton" className="btn col-lg-1">
-                        <Button variant="info" onClick={this.showModal} className="form-control">Add Medication</Button>
+                        <Button variant="info" onClick={() => alert('Not Implemented.')} className="form-control">Add Medication</Button>
                     </div>
                     <div id="dischargeButton" className="btn col-lg-1">
                         <Button variant="outline-danger" onClick={this.showModal} className="form-control">Discharge Patient</Button>
                     </div>
                 </Card.Header>
                 <Card.Body className="text-left">
-                    <Card.Title>{`Triaged on: ${this.state.patient.triageDate}`}</Card.Title>
-                    <Card.Text>{`Date of Birth: ${this.state.patient.dateOfBirth}`}</Card.Text>
-                    <Card.Text>{`Email Address: ${this.state.patient.email}`}</Card.Text>
-                    <Card.Text>{`Telephone Number: ${this.state.patient.phoneNumber}`}</Card.Text>
-                    <AllergiesList allergies={this.state.allergies}/>
+                    <PersonalDetails editDetails={this.state.editDetails} patient={this.state.patient} allergies={this.state.allergies}
+                                     handleEmailChange={this.handleEmailChange} handlePhoneNumberChange={this.handlePhoneNumberChange} addAllergyField={this.addAllergyField}
+                                     addAllergyValue={this.addAllergyValue} removeAllergyField={this.removeAllergyField}/>
                 </Card.Body>
                 <Card.Footer className="text-muted">
                 
